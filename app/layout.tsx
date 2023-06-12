b7c646bf8005c061c3abff6be3992e1e2722cb31
Felix Wotschofsky <hello@felisk.io>
2023-06-12 13:47:18 +0200

    Configure code style ✨

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 'use client';
 
+import { SessionProvider } from 'next-auth/react';
 import { Inter } from 'next/font/google';
 
-import { SessionProvider } from 'next-auth/react';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
