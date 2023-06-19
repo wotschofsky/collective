b07e1d7b38cd6a92cc6b56aec341daf2e8ccc8b0
Felix Wotschofsky <hello@felisk.io>
2023-06-19 12:05:51 +0200

    Customize 404 page 👻

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+import type { FC } from 'react';
+
+const NotFoundPage: FC<{}> = () => (
+  <div className="my-16 text-center">
+    <span className="text-5xl">404</span>
+    <h1 className="mt-4 text-2xl">Page Not Found</h1>
+  </div>
+);
+
+export default NotFoundPage;
