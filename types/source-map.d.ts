0c20446299659eeb94c80f3bb3eb5e848813961b
Felix Wotschofsky <hello@felisk.io>
2023-06-24 11:23:41 +0200

    Replace types for markdown-it-source-map 🔀

--- types/source-map.d.ts
+++
@@ -1,7 +0,0 @@
-// TODO Replace with @types/source-map once merged and released
-declare module 'markdown-it-source-map' {
-  import type MarkdownIt = require('markdown-it');
-
-  const markdownItSourceMap = MarkdownIt.SimplePlugin;
-  export default markdownItSourceMap;
-}
